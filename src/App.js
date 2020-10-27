e5e73ffe807d4196c4a450c7ff21cabd3a70cd16
Gab Camba <gabcamba@outlook.com>
2020-10-27 21:15:37 +0800

    login page, login page styling, authentication via Spotify Auth API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import Player from './Player'
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Spotify Clone!</h1>
-    </div>
-  );
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log(user);
+      })
+    }
+  }, []);
+
+  return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
