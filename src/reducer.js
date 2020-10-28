ba6b246d3c2e897a3210b68547c93ebea444e806
Gab Camba <gabcamba@outlook.com>
2020-10-28 08:40:02 +0800

    fetch playlists

--- src/reducer.js
+++ src/reducer.js
@@ -4,9 +4,10 @@ export const initialState = {
   playing: false,
   item: null,
   playlists: null,
+  token: null
   //remove after developing
-  token:
-    "BQBZK8ZG1cP0KRmNriw7auhaAfZKhl1QDz8pOfPNwOfzEGSmkXBkiogJyHzURdilJiJNsRgLhG7N5uRES_d1cco7X5djSClpW_5TGNZ4Z9XYN2S18ZtcvMrhGUEZmPn2VI_VooyMLUvk4U9JhdnTbIS5_JYFifFb",
+//   token:
+//     "BQBZK8ZG1cP0KRmNriw7auhaAfZKhl1QDz8pOfPNwOfzEGSmkXBkiogJyHzURdilJiJNsRgLhG7N5uRES_d1cco7X5djSClpW_5TGNZ4Z9XYN2S18ZtcvMrhGUEZmPn2VI_VooyMLUvk4U9JhdnTbIS5_JYFifFb",
 };
 
 const reducer = (state, action) => {
@@ -26,7 +27,7 @@ const reducer = (state, action) => {
     case "SET_PLAYLISTS":
       return {
         ...state,
-        playlists: playlists,
+        playlists: action.playlists,
       };
     default:
       return state;
